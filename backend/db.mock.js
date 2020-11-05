8d9ca9864e05510723ae094ad01475da8ea981af
daviderl99 <daviderlich7@hot.ee>
2020-11-05 21:55:16 +0200

    init commit

---
+++ backend/db.mock.js
@@ -0,0 +1,16 @@
+const DATABASE = [
+  {
+    "_id": 0,
+    "imgSrc": null,
+    "caption": null,
+    "bool": false,
+    "correctAnswer": null
+  },
+  {
+    "_id": 1,
+    "imgSrc": null,
+    "caption": null,
+    "bool": true,
+    "correctAnswer": null
+  }
+];
