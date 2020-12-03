7cda942b318535200fafa506d94e6e1336ff0ffc
daviderl99 <daviderlich7@hot.ee>
2020-12-03 12:42:48 +0200

    Added CSS, creators, instructions

--- backend/db.mock.js
+++ backend/db.mock.js
@@ -1,16 +1,37 @@
 const DATABASE = [
   {
     "_id": 0,
-    "imgSrc": "../assets/eiffel_test_img.png",
+    "imgSrc": "https://image.prntscr.com/image/nYYJkEqkRkWnJ63Dp9eFjQ.png",
     "caption": "The Eiffel Tower being built",
     "bool": true,
     "correctAnswer": null
   },
   {
     "_id": 1,
-    "imgSrc": "../assets/general_executing.png",
+    "imgSrc": "https://image.prntscr.com/image/4WfnIDxXTJaKx1L3iMUJqw.png",
     "caption": "War criminal executing civilian",
     "bool": false,
     "correctAnswer": "The shooter (General Nguyen Ngoc Loan) was executing a ruthless Viet Cong assassin (Nguyen Van Lem aka Bay Lop), who had led a team that had targeted the general himself."
+  },
+  {
+    "_id": 2,
+    "imgSrc": "https://image.prntscr.com/image/z3A0G3LYTmuHLtdLWDRMyg.png",
+    "caption": "Chinese protester standing in front of People's Liberation Army tanks",
+    "bool": true,
+    "correctAnswer": null
+  },
+  {
+    "_id": 3,
+    "imgSrc": "https://image.prntscr.com/image/ZOU8jTJERwSiIqtNIR_0qg.png",
+    "caption": "U.S. troops raising a flag in Iwo Jima during World War II",
+    "bool": true,
+    "correctAnswer": null
+  },
+  {
+    "_id": 4,
+    "imgSrc": "https://image.prntscr.com/image/McWQQgVST9uZIx737fadQA.png",
+    "caption": "insert some false caption here",
+    "bool": false,
+    "correctAnswer": "Death of a soldier during the Spanish Civil War."
   }
 ];
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,51 @@
+const IMAGE_SEARCH_URL = "https://www.google.com/searchbyimage?site=search&sa=X&image_url=";
+let object;
 usedList = [];
+
+gamePoints = 0;
+imagesShown = 0;
 
 window.onload = () => {
   pickRandomImage();
 }
 
 const pickRandomImage = () => {
-  let obj = DATABASE[Math.floor(Math.random() * DATABASE.length)];
-  if (!usedList.includes(obj._id)){
-    document.getElementById("image").src = obj.imgSrc;
-    document.getElementById("caption").innerText = obj.caption;
-    usedList.push(obj._id);
+  object = DATABASE[Math.floor(Math.random() * DATABASE.length)];
+  if (!usedList.includes(object._id)){
+    document.getElementById("image").src = object.imgSrc;
+    document.getElementById("caption").innerText = object.caption;
+    document.getElementById("reverse-image-search").href = IMAGE_SEARCH_URL + object.imgSrc;
+    usedList.push(object._id);
+
+    imagesShown++;
+  } else {
+    pickRandomImage();
   }
 }
 
-const checkAnswer = (answer) => {
-  console.log(answer);
-  pickRandomImage();
+const submitAnswer = (answer) => {
+  if (answer === object.bool){
+    // alert("correct");
+    gamePoints++;
+  } else {
+    // alert("incorrect");
+  }
+
+  if (imagesShown >= 5){
+    gameOver();
+  } else {
+    pickRandomImage();
+  }
+}
+
+function gameOver(){
+  alert(`Points: ${gamePoints}/${imagesShown}`);
+}
+
+function copyText(){
+  let text = document.getElementById("caption");
+  text.select();
+  text.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
 }
